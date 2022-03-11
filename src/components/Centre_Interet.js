c1884c60bcc87ffea167b999145a4093daeedfaa
BelinAnthony <anthony.belin@iut-dijon.u-bourgogne.fr>
2022-03-11 12:00:57 +0100

    ajout logo

--- src/components/Centre_Interet.js
+++ src/components/Centre_Interet.js
@@ -2,16 +2,16 @@ import { View, StyleSheet, Image } from "react-native";
 
 export default function Centre_Interet(props) {
     let link = '';
-    if (props.type == '1'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '2'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '3'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '4'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '5'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '6'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '7'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '8'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '9'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
-    if (props.type == '10'){link = require('../../assets/icons/android/4x/helpsxxxhdpi.png')};
+    if (props.type == '1'){link = require('../../assets/icons/android/4x/ci-1xxxhdpi.png')};
+    if (props.type == '2'){link = require('../../assets/icons/android/4x/ci-2xxxhdpi.png')};
+    if (props.type == '3'){link = require('../../assets/icons/android/4x/ci-3xxxhdpi.png')};
+    if (props.type == '4'){link = require('../../assets/icons/android/4x/ci-4xxxhdpi.png')};
+    if (props.type == '5'){link = require('../../assets/icons/android/4x/ci-5xxxhdpi.png')};
+    if (props.type == '6'){link = require('../../assets/icons/android/4x/ci-6xxxhdpi.png')};
+    if (props.type == '7'){link = require('../../assets/icons/android/4x/ci-7xxxhdpi.png')};
+    if (props.type == '8'){link = require('../../assets/icons/android/4x/ci-8xxxhdpi.png')};
+    if (props.type == '9'){link = require('../../assets/icons/android/4x/ci-9xxxhdpi.png')};
+    if (props.type == '10'){link = require('../../assets/icons/android/4x/ci-10xxxhdpi.png')};
 
     return (
         <View style={styles.container}>
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -8,6 +8,15 @@ export default function SettingsScreen() {
         <View style={styles.page}>
             <MonHeader></MonHeader>
             <Centre_Interet type='1'></Centre_Interet>
+            <Centre_Interet type='2'></Centre_Interet>
+            <Centre_Interet type='3'></Centre_Interet>
+            <Centre_Interet type='4'></Centre_Interet>
+            <Centre_Interet type='5'></Centre_Interet>
+            <Centre_Interet type='6'></Centre_Interet>
+            <Centre_Interet type='7'></Centre_Interet>
+            <Centre_Interet type='8'></Centre_Interet>
+            <Centre_Interet type='9'></Centre_Interet>
+            <Centre_Interet type='10'></Centre_Interet>
         </View>
     )
 }
